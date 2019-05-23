0f070896044123d5bcd7e2ba3d7983ed87e530f1
Judit Horvath <judit.of.horvath@gmail.com>
2019-05-23 13:06:43 +0200

    feat(DocOfficeRepos): add findById()

--- src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/doctorsOffice/DoctorsOfficeRepository.java
+++ src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/doctorsOffice/DoctorsOfficeRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DoctorsOfficeRepository extends CrudRepository<DoctorsOffice, Long> {
+  DoctorsOffice findById(long id);
 }
