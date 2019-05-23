18c63888a4888574c058f21e0f647f84f9de3d57
Zoltan Marlok <m.zoltaan@gmail.com>
2019-05-23 13:55:37 +0200

    refactor(UserServiceImpl):create implementation for UserService

--- src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/user/UserService.java
+++ src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/user/UserService.java
@@ -1,15 +1,5 @@
 package com.greenfoxacademy.javaandthescripts.thepatientpatient.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface UserService {
 
-@Service
-public class UserService {
-
-  UserRepository userRepository;
-
-  @Autowired
-  public UserService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
 }
---
+++ src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/user/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.javaandthescripts.thepatientpatient.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+  UserRepository userRepository;
+
+  @Autowired
+  public UserServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+}
