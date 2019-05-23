d769a7a437e28c331d602521b6101196bad5a38d
Judit Horvath <judit.of.horvath@gmail.com>
2019-05-23 13:36:17 +0200

    feat(ServiceServiceImpl): minor changes

--- src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/Service/ServiceServiceImpl.java
+++ src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/Service/ServiceServiceImpl.java
@@ -1,4 +1,7 @@
 package com.greenfoxacademy.javaandthescripts.thepatientpatient.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceServiceImpl implements ServiceService {
 }
