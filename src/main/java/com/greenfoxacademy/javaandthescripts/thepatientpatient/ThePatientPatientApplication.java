de060b8c0129aecfc9d47b9b7225264445e504e7
birobotond20 <biro.botond20@gmail.com>
2019-05-23 11:43:27 +0200

    feat: initialize project

---
+++ src/main/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/ThePatientPatientApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.javaandthescripts.thepatientpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePatientPatientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThePatientPatientApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/greenfoxacademy/javaandthescripts/thepatientpatient/ThePatientPatientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.javaandthescripts.thepatientpatient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThePatientPatientApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
